5fec102d7e32ca03cf3faec13257e69dae301698
toandovan <toan.do.khmtk17@hcmut.edu.vn>
2022-07-14 15:57:41 +0700

    3 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
+import { useEffect, useState } from 'react';
+import './App.css';
+import { Loader } from '@googlemaps/js-api-loader';
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const loader = new Loader({
+    apiKey: '',
+    version: 'weekly',
+    libraries: ['places'],
+  });
+  useEffect(() => {
+    loader.loadCallback((e) => {
+      if (e) {
+        console.log(e);
+      } else {
+        new google.maps.Map(document.getElementsByClassName('map'), mapOptions);
+      }
+    });
+  });
+  const mapOptions = {
+    center: {
+      lat: 0,
+      lng: 0,
+    },
+    zoom: 4,
+  };
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  return <div className="Map"></div>;
 }
 
-export default App
+export default App;
